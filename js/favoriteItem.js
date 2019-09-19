a779bd7d2dc39384fa79fcb1d842cdd34aed04db
Alexey Sladkomodov <aleexxyy@gmail.com>
2019-09-19 18:05:41 +0300

    final

---
+++ js/favoriteItem.js
@@ -0,0 +1,17 @@
+const row = document.querySelector(".row");
+window.addEventListener('load', createFavore)
+    
+async function createFavore() {
+    let linkObj = JSON.parse(localStorage.getItem('itemObj'));
+    let objFavorite;
+    
+    if (linkObj) {
+        objFavorite = [];
+        for (let i = 0; i < linkObj.length; i++){
+            let linkAPI = await fetch(` http://api.tvmaze.com/lookup/shows?thetvdb=${linkObj[i]}`);
+            let linkJson = await linkAPI.json();
+            objFavorite.push(linkJson);
+        }
+        let prod = new FavoriteProduction(row, objFavorite).create('delete', deleteFavore);
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,21 @@
+    const nameSearch = document.querySelector(".serch, input");
+    const buttonSearch = document.querySelector('#button');
+    const row = document.querySelector(".row");
+    /////////// to event
+    buttonSearch.addEventListener('click', search);
+    
+    async function search(){
+        row.innerHTML = "";
+        let linkAPI = await fetch(`http://api.tvmaze.com/search/shows?q=${nameSearch.value}`);
+        let linkJson = await linkAPI.json();
+        let reatCatalog = new ServiceProduction(row, linkJson);
+        reatCatalog.create('Follow', addFavore);
+        reatCatalog.createFilter();
+        sessionStorage.setItem('valueSearch', nameSearch.value);
+    }
+
+    window.addEventListener('load', () => {
+        if (!sessionStorage.getItem('valueSearch')) return;
+        nameSearch.value = sessionStorage.getItem('valueSearch');
+        buttonSearch.click();
+    })
