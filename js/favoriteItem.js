a3a34398b1d5bb7d91dc3c21239b6f09af46e788
aleexxyy <30913058+aleexxyy@users.noreply.github.com>
2019-09-20 10:34:00 +0300

    Update favoriteItem.js

--- js/favoriteItem.js
+++ js/favoriteItem.js
@@ -8,7 +8,7 @@ async function createFavore() {
     if (linkObj) {
         objFavorite = [];
         for (let i = 0; i < linkObj.length; i++){
-            let linkAPI = await fetch(` http://api.tvmaze.com/lookup/shows?thetvdb=${linkObj[i]}`);
+            let linkAPI = await fetch(` https://api.tvmaze.com/lookup/shows?thetvdb=${linkObj[i]}`);
             let linkJson = await linkAPI.json();
             objFavorite.push(linkJson);
         }
